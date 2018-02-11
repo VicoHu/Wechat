7ba80cb4ae770ee0182f0bcdab6be63d664e0566
unknown <956885619@qq.com>
2018-02-11 22:15:29 +0800

    litter change

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ itchat.auto_login()         #enableCmdQR=2
 # itchat.run()
 print("**********")
 Friends_ForName = itchat.search_friends()
-print(Friends['NickName'])
+print(Friends_ForName['NickName'])
 
 
 
